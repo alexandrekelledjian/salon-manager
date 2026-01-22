9abcd50b0360f7b9304dd5bd437dcc4950342b31
alexandrekelledjian <alexandre.kelledjian@depotsgemmes.com>
2026-01-22 17:10:44 +0100

    Lot 3: Add products route to publicRoutes.js

--- api/src/routes/publicRoutes.js
+++ api/src/routes/publicRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getSalonByCode } = require('../controllers/publicController');
+const { getSalonByCode, getProductsBySalonCode } = require('../controllers/publicController');
 
 const router = express.Router();
 
 router.get('/salons/by-code/:code_url', getSalonByCode);
+
+router.get('/salons/:code_url/products', getProductsBySalonCode);
 
 module.exports = router;
